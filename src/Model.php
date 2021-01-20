8efd3677eb8c5127a178396ab5d0f4bc3e91fa4d
Albert <amuluowin@163.com>
2021-01-21 01:00:47 +0800

    update

--- src/Model.php
+++ src/Model.php
@@ -19,6 +19,7 @@ use Rabbit\Base\Core\StaticInstanceTrait;
 use Rabbit\Base\Exception\InvalidArgumentException;
 use Rabbit\Base\Exception\InvalidConfigException;
 use Rabbit\Base\Helper\Inflector;
+use Rabbit\Model\Model as ModelModel;
 use ReflectionClass;
 use ReflectionException;
 use Respect\Validation\Validatable;
@@ -59,7 +60,7 @@ use Throwable;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class Model extends BaseObject implements StaticInstanceInterface, IteratorAggregate, ArrayAccess, Arrayable
+class Model extends ModelModel implements StaticInstanceInterface, IteratorAggregate, ArrayAccess, Arrayable
 {
     use ArrayableTrait;
     use StaticInstanceTrait;
@@ -264,7 +265,7 @@ class Model extends BaseObject implements StaticInstanceInterface, IteratorAggre
      * @return bool whether the validation is successful without any error.
      * @throws InvalidArgumentException if the current scenario is unknown.
      */
-    public function validate(array $attributeNames = null, bool $clearErrors = true)
+    public function validate(array $attributeNames = null, bool $throwAble = true, bool $firstReturn = false, bool $clearErrors = true): bool
     {
         if ($clearErrors) {
             $this->clearErrors();
@@ -315,83 +316,6 @@ class Model extends BaseObject implements StaticInstanceInterface, IteratorAggre
     }
 
     /**
-     * Returns a value indicating whether there is any validation error.
-     * @param string|null $attribute attribute name. Use null to check all attributes.
-     * @return bool whether there is any error.
-     */
-    public function hasErrors(string $attribute = null): bool
-    {
-        return $attribute === null ? !empty($this->_errors) : isset($this->_errors[$attribute]);
-    }
-
-    /**
-     * Returns the errors for all attributes or a single attribute.
-     * @param string $attribute attribute name. Use null to retrieve errors for all attributes.
-     * @return array errors for all attributes or the specified attribute. Empty array is returned if no error.
-     * Note that when returning errors for all attributes, the result is a two-dimensional array, like the following:
-     *
-     * ```php
-     * [
-     *     'username' => [
-     *         'Username is required.',
-     *         'Username must contain only word characters.',
-     *     ],
-     *     'email' => [
-     *         'Email address is invalid.',
-     *     ]
-     * ]
-     * ```
-     *
-     * @property array An array of errors for all attributes. Empty array is returned if no error.
-     * The result is a two-dimensional array. See [[getErrors()]] for detailed description.
-     * @see getFirstErrors()
-     * @see getFirstError()
-     */
-    public function getErrors(string $attribute = null): array
-    {
-        if ($attribute === null) {
-            return $this->_errors ?? [];
-        }
-
-        return isset($this->_errors[$attribute]) ? $this->_errors[$attribute] : [];
-    }
-
-    /**
-     * Returns the first error of every attribute in the model.
-     * @return array the first errors. The array keys are the attribute names, and the array
-     * values are the corresponding error messages. An empty array will be returned if there is no error.
-     * @see getErrors()
-     * @see getFirstError()
-     */
-    public function getFirstErrors(): array
-    {
-        if (empty($this->_errors)) {
-            return [];
-        }
-
-        $errors = [];
-        foreach ($this->_errors as $name => $es) {
-            if (!empty($es)) {
-                $errors[$name] = reset($es);
-            }
-        }
-
-        return $errors;
-    }
-
-    /**
-     * Returns the first error of the specified attribute.
-     * @param string $attribute attribute name.
-     * @return string the error message. Null is returned if no error.
-     * @see getErrors()
-     * @see getFirstErrors()
-     */
-    public function getFirstError(string $attribute): ?string
-    {
-        return isset($this->_errors[$attribute]) ? reset($this->_errors[$attribute]) : null;
-    }
-
-    /**
      * Returns the errors for all attributes as a one-dimensional array.
      * @param bool $showAllErrors boolean, if set to true every error message for each attribute will be shown otherwise
      * only the first error message for each attribute will be shown.
@@ -408,50 +332,6 @@ class Model extends BaseObject implements StaticInstanceInterface, IteratorAggre
             $lines = array_merge((array)$es, $lines);
         }
         return $lines;
-    }
-
-    /**
-     * Adds a new error to the specified attribute.
-     * @param string $attribute attribute name
-     * @param string $error new error message
-     */
-    public function addError(string $attribute, string $error = ''): void
-    {
-        $this->_errors[$attribute][] = $error;
-    }
-
-    /**
-     * Adds a list of errors.
-     * @param array $items a list of errors. The array keys must be attribute names.
-     * The array values should be error messages. If an attribute has multiple errors,
-     * these errors must be given in terms of an array.
-     * You may use the result of [[getErrors()]] as the value for this parameter.
-     * @since 2.0.2
-     */
-    public function addErrors(array $items): void
-    {
-        foreach ($items as $attribute => $errors) {
-            if (is_array($errors)) {
-                foreach ($errors as $error) {
-                    $this->addError($attribute, $error);
-                }
-            } else {
-                $this->addError($attribute, $errors);
-            }
-        }
-    }
-
-    /**
-     * Removes errors for all attributes or a single attribute.
-     * @param string $attribute attribute name. Use null to remove errors for all attributes.
-     */
-    public function clearErrors(string $attribute = null): void
-    {
-        if ($attribute === null) {
-            $this->_errors = [];
-        } else {
-            unset($this->_errors[$attribute]);
-        }
     }
 
     /**
